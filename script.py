932f170d7da91cc7df941a9a74fdddaefc605b45
Anton Oboleninov <oboleninoff.anton@yandex.ru>
2023-03-19 20:41:24 +0700

    Add space

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 
 from tree import DecisionTree
 
-data = pd.read_csv("data/cardio_train_small.csv", sep=";")
+data = pd.read_csv("data/cardio_train_medium.csv", sep=";")
 
 new_data = data.drop(['id', 'cardio', 'age'], axis=1)
 data, target = new_data.to_numpy(), np.array(data["cardio"])
@@ -12,6 +12,7 @@ data, target = new_data.to_numpy(), np.array(data["cardio"])
 X_train, X_test, y_train, y_test = train_test_split(
     data, target, test_size=0.3, random_state=2023,
 )
+
 print(f"{X_train=}")
 print(f"{X_test=}")
 print(f"{y_train=}")
